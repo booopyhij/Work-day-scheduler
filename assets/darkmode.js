02bfa2b0ed7a76a900fe3a8582ec314814fb5f51
booopyhij <mphillips2017@gmail.comgit config --global user.name booopyhijgit config --global user.email mphillips2017@gmail.com>
2023-06-05 16:03:57 -0400

    made comments on the code

--- assets/darkmode.js
+++ assets/darkmode.js
@@ -11,3 +11,4 @@ $('.change').on('click', function(){
 //this code is targeting the 'change class' upon clicking a button
 // allowing the user to be
 //able to switch between dark and light mode for better viewing
+// using and if {}else {} statement to define the parameters
